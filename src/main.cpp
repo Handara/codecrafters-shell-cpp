5c99a96f04cff0e5bda20b137d4d5de0f504ffc2
Handara <rachidhandaoui@gmail.com>
2026-05-08 17:32:44 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
 
+
+bool is_string_included_in_array(std::string *haystack, std::string needle, size_t size){
+  for(int i = 0; i < size; i++){
+    if (needle == haystack[i]){
+      return true;
+    }
+  }
+  return false;
+}
+
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
   std::string command;
+  std::string builtins[] = {"exit","echo","type"};
   while(true){
     std::cout << "$ ";
     std::getline(std::cin, command);
@@ -13,7 +25,15 @@ int main() {
       break;
     }else if(command.substr(0,5) == "echo "){
       std::cout << command.substr(5) << std::endl;
-    }else {
+    }else if(command.substr(0,5) == "type "){
+      std::string subcommand_type = command.substr(5);
+      if (is_string_included_in_array(builtins,subcommand_type,3)){
+        std::cout << subcommand_type << " is a shell builtin.\n"; 
+      } else{
+        std::cout << subcommand_type << ": command not found\n";
+      }
+    }
+    else {
       std::cout << command << ": command not found\n";
     }
   }
