54fea12744a16c947ab545fc257eef0b42fa62a2
Handara <rachidhandaoui@gmail.com>
2026-05-08 16:57:41 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,9 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  std::string command;
   while(true){
     std::cout << "$ ";
-    std::string command;
     std::getline(std::cin, command);
     std::cout << command << ": command not found\n";
   }
