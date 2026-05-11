88c927a35606f7c013cee5ce54d64980fdca0cee
Handara <rachidhandaoui@gmail.com>
2026-05-11 21:34:23 +0100

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <unistd.h>
 #include <cstdlib>
-
+#include <vector>
+#include <sys/wait.h>
 bool is_string_included_in_array(std::string *haystack, std::string needle, size_t size){
   for(int i = 0; i < size; i++){
     if (needle == haystack[i]){
@@ -66,17 +67,30 @@ int main() {
         size_t start = 0;
         size_t end = command.find(' ', start);
         std::string last_output = "";
+        std::vector<char *> argv;
         while(end != std::string::npos){
           end = command.find(' ', start);
           if (arg_count == 0){
             last_output += "Arg #" + std::to_string(arg_count) + " (program name): " + command.substr(start,end-start) + "\n";
           }else{
-            last_output += "Arg #" + std::to_string(arg_count) + ": " + command.substr(start,end-start) + "\n";
+            std::string arg = command.substr(start,end-start);
+            last_output += "Arg #" + std::to_string(arg_count) + ": " + arg + "\n";
+            argv.push_back(arg.data());
           }
           arg_count++;
           start = end + 1;
         }
-        std::cout << "Program was passed " << arg_count <<" args (including program name)." << "\n" << last_output << "Program Signature: "<< std::system(first_command.c_str()) << "\n";
+        std::cout << "Program was passed " << arg_count <<" args (including program name)." << "\n" << last_output ;
+        pid_t pid = fork();
+        if (pid == 0) {
+            // child process
+            std::cout << "Program Signature: " << execv(first_command.c_str(), argv.data()) << "\n";
+            exit(1);
+        } else {
+            // parent process 
+            
+            waitpid(pid, nullptr, 0);
+        }
       }else{
         std::cout << command << ": command not found\n";
       }
